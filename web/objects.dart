a377dc415d644e6a116d2b5879e850b02a8e9c56
Sam Gruber <grubermensch@gmail.com>
2013-07-28 21:27:37 -0400

    Move BoundingBox and BoundedObject to objects.dart

--- web/objects.dart
+++ web/objects.dart
@@ -1,8 +1,6 @@
 import 'dart:html';
 import 'dart:math';
 
-import 'quadtree.dart' show BoundingBox;
-
 class Vec2 {
   num x, y;
   Vec2(this.x, this.y);
@@ -63,6 +61,58 @@ class LocusByCoords extends DrawingObject {
   BoundingBox get bounds {
     return new BoundingBox(integerPosition, integerPosition);
   }
+}
+
+/* BoundedObject stores a DrawingObject with its bounding box for quick
+ * reference by the QuadTree structure.
+ */
+class BoundedObject {
+  DrawingObject obj;
+  BoundingBox bb;
   
+  BoundedObject(this.obj){
+    this.bb = this.obj.bounds;
+  }
   
+  /* Returns true if the bounding box intersects the object */
+  bool intersects(BoundingBox searchBB) {
+    // Short-circuit evaluation means we only do the expensive check after
+    // cheaply checking the bounding box.
+    return bb.intersects(searchBB) && obj.intersects(searchBB);
+  }
+}
+
+/* BoundingBox is an axis-aligned bounding box */
+class BoundingBox {
+  Point ne, sw;
+  
+  BoundingBox(this.ne, this.sw);
+  
+  /* Returns true if the box is inside our bounding box, false otherwise */
+  bool contains(BoundingBox searchBB) {
+    return (ne.x >= searchBB.ne.x && ne.y >= searchBB.ne.y &&
+        sw. x <= searchBB.sw.x && sw.y <= searchBB.sw.y);
+  }
+  
+  /* Returns true if the supplied bounding box intersects this box */
+  bool intersects(BoundingBox searchBB) {
+    return (
+      (searchBB.sw.y <= this.ne.y) &&
+      (searchBB.ne.y >= this.sw.y) &&
+      (searchBB.sw.x <= this.ne.x) &&
+      (searchBB.ne.x >= this.sw.x)
+    );
+  }
+  
+  /* Returns the tightest bounding box which completely contains the two given
+   * bounding boxes.
+   */
+  static BoundingBox union(BoundingBox bb1, BoundingBox bb2) {
+    num north = max(bb1.ne.y, bb2.ne.y);
+    num south = min(bb1.sw.y, bb2.sw.y);
+    num east = max(bb1.ne.x, bb2.ne.x);
+    num west = min(bb1.sw.x, bb2.sw.x);
+    
+    return new BoundingBox(new Point(east, north), new Point(west, south));
+  }
 }
--- web/quadtree.dart
+++ web/quadtree.dart
@@ -2,7 +2,7 @@ library quadtree;
 
 import 'dart:math';
 import 'dart:html' show Point;
-import 'objects.dart' show DrawingObject;
+import 'objects.dart' show BoundingBox, BoundedObject;
 
 const Point initialQuadtreeOrigin = const Point(0,0);
 const num initialQuadtreeDim = 64;
@@ -246,63 +246,4 @@ class QuadTreeNode {
     
     return out;
   }
-  
-}
-
-/* BoundedObject stores a DrawingObject with its bounding box for quick
- * reference by the QuadTree structure.
- */
-class BoundedObject {
-  DrawingObject obj;
-  BoundingBox bb;
-  
-  BoundedObject(this.obj){
-    this.bb = this.obj.bounds;
-  }
-  
-  /* Returns true if the bounding box intersects the object */
-  bool intersects(BoundingBox searchBB) {
-    // Short-circuit evaluation means we only do the expensive check after
-    // cheaply checking the bounding box.
-    return bb.intersects(searchBB) && obj.intersects(searchBB);
-  }
-}
-
-/* BoundingBox is an axis-aligned bounding box */
-class BoundingBox {
-  Point ne, sw;
-  
-  BoundingBox(this.ne, this.sw);
-  
-  /* Returns true if the box is inside our bounding box, false otherwise */
-  bool contains(BoundingBox searchBB) {
-    return (ne.x >= searchBB.ne.x && ne.y >= searchBB.ne.y &&
-        sw. x <= searchBB.sw.x && sw.y <= searchBB.sw.y);
-  }
-  
-  /* Returns true if the supplied bounding box intersects this box */
-  bool intersects(BoundingBox searchBB) {
-    return (
-      (searchBB.sw.y <= this.ne.y) &&
-      (searchBB.ne.y >= this.sw.y) &&
-      (searchBB.sw.x <= this.ne.x) &&
-      (searchBB.ne.x >= this.sw.x)
-    );
-  }
-  
-  /* Returns the tightest bounding box which completely contains the two given
-   * bounding boxes.
-   */
-  static BoundingBox union(BoundingBox bb1, BoundingBox bb2) {
-    num north = max(bb1.ne.y, bb2.ne.y);
-    num south = min(bb1.sw.y, bb2.sw.y);
-    num east = max(bb1.ne.x, bb2.ne.x);
-    num west = min(bb1.sw.x, bb2.sw.x);
-    
-    return new BoundingBox(new Point(east, north), new Point(west, south));
-  }
-}
-
-bool almostequal(num a, num b) {
-  return (a-b < 0.00001);
 }
