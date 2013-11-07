7b2c4d0c5f8df8b8b62bdc96624ed01b2d20cbd0
Sam Gruber <grubermensch@gmail.com>
2013-11-07 15:36:10 -0500

    start over

--- web/construct.dart
+++ web/construct.dart
@@ -1,21 +1,6 @@
 // Imports
 import 'dart:html';
-import 'draw.dart';
-import 'objects.dart';
-import 'toolbar.dart';
 
 void main() {
-  // Initialize the workspace canvas element
-  CanvasElement wsCvs = query("#workspace");
-  gRedrawManager = new RedrawManager(wsCvs.getContext("2d"));
-  gRedrawManager.addDisplayObject(new LocusByCoords(new Point(0, 0)));
-  gRedrawManager.addPreselectionObject(new LocusByCoords(new Point(0, -50)));
-  gRedrawManager.addSelectionObject(new LocusByCoords(new Point(0, 50)));
-  gRedrawManager.redraw();
-  
-  //Initialize the toolbar canvas element
-  CanvasElement tbCvs = query("#toolbar");
-  gToolbarManager = new ToolbarManager(tbCvs.getContext("2d"));
-  registerAllTools();
-  gToolbarManager.redraw();
+
 }
