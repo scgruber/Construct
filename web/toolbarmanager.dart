8be71f53dad572e1f43ffccccd6b075559c5d2be
Sam Gruber <grubermensch@gmail.com>
2013-11-07 20:01:01 -0500

    write toolbar draw code

--- web/toolbarmanager.dart
+++ web/toolbarmanager.dart
@@ -1,13 +1,59 @@
 library toolbarmanager;
 
+import 'dart:core';
 import 'dart:html';
 
 class ToolBarManager {
   CanvasRenderingContext2D cvs;
+  List<Tool> objectTools;
+  List<Tool> constraintTools;
+  
+  static const num buttonDim = 30;
+  static const num buttonSpacing = 5;
   
   ToolBarManager(this.cvs){
-    // Paint the background gray to initialize
-    this.cvs.fillStyle = "#222222";
+    // Paint the background gray
+    this.cvs.fillStyle = '#222222';
     this.cvs.fillRect(0, 0, this.cvs.canvas.width, this.cvs.canvas.height);
+    
+    // Set up the toolsets
+    this.objectTools = new List();
+    this.constraintTools = new List();
+    
+    // Draw the initial toolbar
+    this.draw();
   }
+  
+  void draw() {
+    num vpos = buttonSpacing;
+    for (var t in objectTools) {
+      t.drawButton(buttonSpacing, vpos, buttonDim, buttonDim);
+      vpos += buttonDim + buttonSpacing;
+    }
+    drawSpacer(buttonSpacing, vpos, buttonDim, buttonSpacing);
+    vpos += buttonSpacing + buttonSpacing;
+    for (var t in constraintTools) {
+      t.drawButton(buttonSpacing, vpos, buttonDim, buttonSpacing);
+      vpos += buttonDim + buttonSpacing;
+    }
+  }
+  
+  void drawSpacer(num x, num y, num width, num height) {
+    cvs.strokeStyle = '#BBBBBB';
+    cvs.lineWidth = 1;
+    cvs.moveTo(x, y);
+    cvs.lineTo(x+width, y);
+    cvs.stroke();
+    cvs.moveTo(x, y+height);
+    cvs.lineTo(x+width, y+height);
+    cvs.stroke();
+  }
+}
+
+abstract class Tool {
+  Tool();
+  void register(ToolBarManager tb);
+  
+  void drawButton(num x, num y, num width, num height);
+  void click();
 }
