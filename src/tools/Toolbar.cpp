2f8a4a03ea529de4d8938aaaeea7f4e6f51b598a
Sam Gruber <grubermensch@gmail.com>
2014-01-02 17:55:45 -0500

    register selection tool

--- src/tools/Toolbar.cpp
+++ src/tools/Toolbar.cpp
@@ -1,4 +1,6 @@
 #include "ofMain.h"
+#include "SelectionTool.h"
+
 #include "Toolbar.h"
 
 Toolbar::Toolbar() {
@@ -6,6 +8,8 @@ Toolbar::Toolbar() {
     for (int iToolSet = 0; iToolSet < mcNumToolSets; iToolSet++) {
         mToolSets[iToolSet] = new std::vector<Tool*>();
     }
+    // Register tools
+    registerTool(0,new SelectionTool());
 }
 
 Toolbar::~Toolbar() {
@@ -15,6 +19,15 @@ Toolbar::~Toolbar() {
 void Toolbar::draw() {
     ofSetColor(200,200,200);
     ofRectRounded(20,20,mWidth,mHeight,5);
+    ofPushMatrix();
+    ofTranslate(25,25,0);
+    for (int iToolSet = 0; iToolSet < mcNumToolSets; iToolSet++) {
+        for (std::vector<Tool*>::iterator it = mToolSets[iToolSet]->begin();
+            it != mToolSets[iToolSet]->end(); it++) {
+            (*it)->drawButton();
+        }
+    }
+    ofPopMatrix();
 }
 
 bool Toolbar::registerTool(int toolGroup, Tool* tool) {
