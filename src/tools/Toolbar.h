7cd13e65ec053154646f6ae2676be2cc04084a4a
Sam Gruber <grubermensch@gmail.com>
2014-01-02 14:32:58 -0500

    fix include syntax

--- src/tools/Toolbar.h
+++ src/tools/Toolbar.h
@@ -1,6 +1,7 @@
 #ifndef TOOLBAR_H
 #define TOOLBAR_H
 
+#include "Tool.h"
 
 class Toolbar {
 public:
@@ -8,8 +9,12 @@ public:
     virtual ~Toolbar();
 
     void draw();
+
+    bool registerTool(int toolGroup, Tool* tool);
 protected:
 private:
+    static const size_t cNumToolSets = 2;
+    std::vector<Tool*>* mToolSets;
 };
 
 #endif // TOOLBAR_H
