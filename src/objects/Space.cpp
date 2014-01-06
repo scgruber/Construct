dd82087c305b3b5dbdcdfe4c4e435d384a8e0302
Sam Gruber <grubermensch@gmail.com>
2014-01-06 12:39:11 -0500

    space stub

---
+++ src/objects/Space.cpp
@@ -0,0 +1,11 @@
+#include "Space.h"
+
+Space::Space()
+{
+    //ctor
+}
+
+Space::~Space()
+{
+    //dtor
+}
---
+++ src/objects/Space.h
@@ -0,0 +1,14 @@
+#ifndef SPACE_H
+#define SPACE_H
+
+
+class Space
+{
+    public:
+        Space();
+        virtual ~Space();
+    protected:
+    private:
+};
+
+#endif // SPACE_H
