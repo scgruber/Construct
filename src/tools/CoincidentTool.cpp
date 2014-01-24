c8923bbb446c512244db49cf7e28f7afc6418225
Sam Gruber <grubermensch@gmail.com>
2014-01-24 13:06:48 -0500

    stub CoincidentTool

---
+++ src/tools/CoincidentTool.cpp
@@ -0,0 +1,29 @@
+#include "CoincidentTool.h"
+
+CoincidentTool::CoincidentTool() {
+    //ctor
+}
+
+CoincidentTool::~CoincidentTool() {
+    //dtor
+}
+
+void CoincidentTool::setUp() {
+
+}
+
+void CoincidentTool::setDown() {
+
+}
+
+void CoincidentTool::handleLeftClick() {
+
+}
+
+void CoincidentTool::drawButton() {
+
+}
+
+void CoincidentTool::drawTool() {
+
+}
---
+++ src/tools/CoincidentTool.h
@@ -0,0 +1,23 @@
+#ifndef COINCIDENTTOOL_H
+#define COINCIDENTTOOL_H
+
+#include "Tool.h"
+
+
+class CoincidentTool : public Tool
+{
+    public:
+        CoincidentTool();
+        virtual ~CoincidentTool();
+
+        void setUp();
+        void handleLeftClick();
+        void setDown();
+
+        void drawButton();
+        void drawTool();
+    protected:
+    private:
+};
+
+#endif // COINCIDENTTOOL_H
